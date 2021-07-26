9b33a025a0b6da81e6631de79c1831103c077747
Josh Coulter <u0861925@unimail.hud.ac.uk>
2021-07-26 18:17:15 +0100

    Working on profile page, header and nav links added

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -1,4 +1,5 @@
 import Avatar from "@material-ui/core/Avatar";
+import Tooltip from "@material-ui/core/Tooltip";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 import type { User } from "@lib/types";
@@ -19,15 +20,22 @@ const useStyles = makeStyles((theme: Theme) =>
       height: theme.spacing(7),
       fontSize: theme.spacing(6),
     },
+    profile: {
+      width: theme.spacing(15),
+      height: theme.spacing(15),
+      fontSize: theme.spacing(13),
+    },
   })
 );
 
 const UserAvatar = ({
   profile,
   size = "medium",
+  tooltip = profile.fullName || "",
 }: {
   profile: User;
   size?: string;
+  tooltip?: string;
 }) => {
   const classes = useStyles();
 
@@ -39,17 +47,23 @@ const UserAvatar = ({
         return classes.medium;
       case "large":
         return classes.large;
+      case "profile":
+        return classes.profile;
       default:
         return classes.medium;
     }
   };
 
-  return profile.profileURL ? (
-    <Avatar src={profile.profileURL} className={getSize()} alt="" />
-  ) : (
-    <Avatar className={getSize()}>
-      {profile.firstName.charAt(0).toUpperCase()}
-    </Avatar>
+  return (
+    <Tooltip title={tooltip}>
+      {profile.profileURL ? (
+        <Avatar src={profile.profileURL} className={getSize()} alt="" />
+      ) : (
+        <Avatar className={getSize()}>
+          {profile.firstName.charAt(0).toUpperCase()}
+        </Avatar>
+      )}
+    </Tooltip>
   );
 };
 
--- pages/users/[user]/index.tsx
+++ pages/users/[user]/index.tsx
@@ -6,11 +6,15 @@ import {
   Theme,
   alpha,
 } from "@material-ui/core/styles";
+import AvatarGroup from "@material-ui/lab/AvatarGroup";
+import Divider from "@material-ui/core/Divider";
 
 import type { User } from "@lib/types";
 import { firestore } from "@lib/firebase";
 import UserAvatar from "@components/UserAvatar";
 import Loader from "@components/Loader";
+import { Avatar, Tooltip } from "@material-ui/core";
+import Link from "next/link";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -29,7 +33,45 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     container: {
       width: "70%",
-      backgroundColor: "grey",
+      // backgroundColor: "grey",
+    },
+    header: {
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    profileSection: {
+      display: "flex",
+      alignItems: "center",
+    },
+    title: {
+      fontSize: "3rem",
+      fontWeight: "bold",
+    },
+    avatars: {
+      width: theme.spacing(3),
+      height: theme.spacing(3),
+      fontSize: theme.spacing(2),
+    },
+    optionsList: {
+      display: "flex",
+      justifyContent: "space-between",
+      textAlign: "center",
+      width: "50%",
+      height: "4rem",
+      lineHeight: "4rem",
+      listStyle: "none",
+      padding: "0",
+    },
+    optionsListItem: {
+      margin: "auto",
+      fontWeight: "bold",
+      width: "100%",
+      height: "100%",
+      borderRadius: "10px",
+      cursor: "pointer",
+      "&:hover": {
+        backgroundColor: "#ccc",
+      },
     },
   })
 );
@@ -41,6 +83,11 @@ const Profile = () => {
   const [user, setUser] = useState<User | null>(null);
   const userId = router.query.user;
   const classes = useStyles();
+
+  let friendList: User[] = [];
+  for (let i = 0; i < 12; i++) {
+    friendList.push(user as User);
+  }
 
   useEffect(() => {
     const getUser = async () => {
@@ -58,6 +105,10 @@ const Profile = () => {
     getUser();
   }, [userId]);
 
+  const Content = () => {
+    return <div>Content</div>;
+  };
+
   return (
     <main className={`${classes.main} ${classes.centerItems}`}>
       <div className={`${classes.container}`}>
@@ -72,7 +123,85 @@ const Profile = () => {
         ) : (
           <div>
             {user ? (
-              <UserAvatar profile={user} size="small" />
+              <div>
+                {/* Header section */}
+                <div className={classes.header}>
+                  <div className={classes.profileSection}>
+                    <UserAvatar profile={user} size="profile" />
+                    <div
+                      style={{
+                        display: "block",
+                        margin: "none",
+                        padding: "30px",
+                        height: "100%",
+                      }}
+                    >
+                      <span className={classes.title}>{user.fullName}</span>
+                      <br />
+                      <Link href={`/users/${userId}/friends`}>Friends: 0</Link>
+                      <AvatarGroup
+                        max={5}
+                        classes={{
+                          root: classes.avatars,
+                          avatar: classes.avatars,
+                        }}
+                      >
+                        <Tooltip title="Josh Coulter">
+                          <Avatar>J</Avatar>
+                        </Tooltip>
+                        <Tooltip title="Josh Coulter">
+                          <Avatar>J</Avatar>
+                        </Tooltip>
+                        <Tooltip title="Josh Coulter">
+                          <Avatar>J</Avatar>
+                        </Tooltip>
+                        <Tooltip title="Josh Coulter">
+                          <Avatar>J</Avatar>
+                        </Tooltip>
+                        <Tooltip title="Josh Coulter">
+                          <Avatar>J</Avatar>
+                        </Tooltip>
+                        <Tooltip title="Josh Coulter">
+                          <Avatar>J</Avatar>
+                        </Tooltip>
+                        <Tooltip title="Josh Coulter">
+                          <Avatar>J</Avatar>
+                        </Tooltip>
+                        <Tooltip title="Josh Coulter">
+                          <Avatar>J</Avatar>
+                        </Tooltip>
+                        <Tooltip title="Josh Coulter">
+                          <Avatar>J</Avatar>
+                        </Tooltip>
+                      </AvatarGroup>
+                    </div>
+                  </div>
+                  <div className={classes.profileSection}>
+                    <UserAvatar profile={user} size="large" />
+                    {user.fullName}
+                  </div>
+                </div>
+                <Divider />
+                {/* Selection Menu section */}
+                <div>
+                  <ul className={classes.optionsList}>
+                    <Link href="#" passHref>
+                      <li className={classes.optionsListItem}>Posts</li>
+                    </Link>
+                    <li className={classes.optionsListItem}>
+                      <Link href="#">About</Link>
+                    </li>
+                    <li className={classes.optionsListItem}>
+                      <Link href="#">Friends</Link>
+                    </li>
+                    <li className={classes.optionsListItem}>
+                      <Link href="#">Photos</Link>
+                    </li>
+                  </ul>
+                </div>
+                {/* Content section */}
+                <Content />
+              </div>
             ) : (
               <Loader show={showLoader} />
             )}
